d0349ac823387b9e22f688b64dffaf684cbfd80d
Simon Nilsson <sim.nilsson@gmail.com>
2015-11-16 21:46:44 +0100

    Enabled anti-aliasing

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -93,6 +93,7 @@ int main(int argc, char * argv[]) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
 
     // Check for Valid Context
